4229d586618b535d0881572f2bb8c2af2a298c85
Xihai Luo <xihai.luo01@gmail.com>
2022-01-31 04:11:05 +0000

    fix tests

--- spec/models/health_region_spec.rb
+++ spec/models/health_region_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe HealthRegion, type: :model do
   describe 'Health region' do
-    before do
+    before(:each) do
       @stage = Stage.create(max_indoor_gathering: 5, max_outdoor_gathering: 10,
         food_establishments: "Indoor dining is closed. Takeout & Outdoor dining: Maximum of 4 people per table exclusing
         large households.",
@@ -18,15 +18,15 @@ RSpec.describe HealthRegion, type: :model do
         10 performers",
         personal_care: "Salons and Barbershops closed to the general public",
         color_code: "red")
-        @healthRegionOne = HealthRegion.create(region_name: "toronto", region_code: 3212, stage_id: 1)
-        @healthRegionTwo = HealthRegion.create(region_name: "london", region_code: 3982, stage_id: 1)
+        @healthRegionOne = @stage.health_regions.create(region_name: "toronto", region_code: 3212)
+        @healthRegionTwo = @stage.health_regions.create(region_name: "london", region_code: 3982)
     end
 
     it 'fetches data in healthRegions table' do
-      code = ["2270", "5183", "2265"]
+      code = ["3212", "3982"]
       i = 0
       HealthRegion.find_each do |healthRegion|
-        if i == 3
+        if i == 2
           break
         end
         expect(healthRegion.region_code).to eq(code[i])
@@ -35,23 +35,19 @@ RSpec.describe HealthRegion, type: :model do
     end
 
     it 'returns an object of PHU_ID (key) and stage_id (value)' do
-      result = {
-        '2270': 1,
-        '5183': 3,
-        '2265': 2
-      }
       obj = {}
       i = 0
       HealthRegion.find_each do |healthRegion|
-        if i == 3
+        if i == 2
           break
         end
         key = healthRegion.region_code
         value = healthRegion.stage_id
+
         obj[key] = value
         i = i + 1
       end
-      expect(obj).to include('2270': 1, '5183': 3, '2265': 2)
+      expect(obj).to include("3212" => @stage.id, "3982" => @stage.id)
     end
   end
 end
