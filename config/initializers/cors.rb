3e5c4831480172b63d3dc684ac01e4379168749f
Xihai Luo <xihai.luo01@gmail.com>
2022-02-02 21:58:51 -0500

    set cookies for /admin/* routes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,6 +3,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://jolly-panini-44dac4.netlify.app'
-    resource '/*', headers: :any, credentials: true, methods: [:get, :post, :patch, :put]
+    resource '/admin/*', headers: :any, credentials: true, methods: [:get, :post, :patch, :put]
   end
 end
