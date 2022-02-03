e88a3e2063697f084ac5f7dc278f9f2b589f5dfd
Xihai Luo <xihai.luo01@gmail.com>
2022-02-02 23:03:15 -0500

    add cors resource for /api routes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,5 +4,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://jolly-panini-44dac4.netlify.app'
     resource '/admin/*', headers: %w[Authorization], expose: ["set-cookie"], credentials: true, methods: [:get, :post, :patch, :put]
+    resource '/api/*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
