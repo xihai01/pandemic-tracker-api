1bd45c79102fc49dbae3eeafbdcede47b8f1b9c4
Xihai Luo <xihai.luo01@gmail.com>
2022-02-03 22:40:42 -0500

    add :delete to cors method

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://jolly-panini-44dac4.netlify.app'
-    resource '/admin/*', headers: %w[Authorization], expose: ["set-cookie"], credentials: true, methods: [:get, :post, :patch, :put]
+    resource '/admin/*', headers: :any, expose: ["set-cookie"], credentials: true, methods: [:get, :post, :delete, :put]
     resource '/api/*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
